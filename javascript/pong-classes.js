b167a4ed42a33dd73a6ee2462169af07089fbaf9
Nubbelium <43620094+Nubbelium@users.noreply.github.com>
2022-01-03 10:44:17 -0600

    Final Update

--- javascript/pong-classes.js
+++ javascript/pong-classes.js
@@ -15,12 +15,12 @@ export class Game {
     computerDifficulty,
     computerDifficultySpeed
   ) {
-    this.size = { x: 600, y: 500 }; //container for entire game
+    this.size = { x: 1000, y: 800 }; //container for entire game
     this.padding = { x: 10, y: 10 }; //white space inside game border
     this.htmlElement = null; //element on HTML page
     this.mousePosition = { x: 0, y: 0 };
     this.mute = false; //default mute state
-    this.volume = 6; //default volume output 0-10
+    this.volume = 7; //default volume output 0-10
     this.computerState = 0; //default computer control amount
     this.computerDifficulty = ["off", "easy", "medium", "hard", "impossible"]; //sets the computer difficulty names
     this.computerDifficultySpeed = [0, 2, 4, 8, 100]; //sets the computer difficulty speeds
@@ -41,7 +41,7 @@ export class Ball {
     this.size = { x: 20, y: 20 }; // default ball size
     this.position = { x: 50, y: 50 }; // default ball position
     this.direction = { x: 5, y: 0 }; //Ball speed when game starts
-    this.acceleration = 0; //X speed multipler on successful block
+    this.acceleration = 1.2; //X speed multipler on successful block
     this.initial = { x: 2, y: 0 }; //speed reset on point score
     this.maximum = { x: 20, y: 10 }; //maximum speed
     this.inertialTransfer = 0.25; //multiplier for velocity.y transfer from paddle
@@ -60,7 +60,7 @@ export class Paddle {
     hasHit,
     htmlElement
   ) {
-    this.size = { x: 10, y: 75 }; //default paddle size
+    this.size = { x: 10, y: 125 }; //default paddle size
     this.position = { x: 50, y: 50 }; //paddle position, (Y is top)
     this.oldPosition = { x: 50, y: 50 }; //paddle position 1 frame ago
     this.velocity = { x: 0, y: 0 }; //calculated velocity
